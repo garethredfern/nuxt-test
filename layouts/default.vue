30d25a347f978000f7db2156fbeb8e1bab6ebde5
Gareth Redfern <gareth@garethredfern.com>
2022-01-30 08:26:38 +0000

    initial commit

---
+++ layouts/default.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <header class="p-5">
+      Logo
+    </header>
+    <main>
+      <slot />
+    </main>
+  </div>
+</template>
+
+<script lang="ts" setup>
+useMeta({
+  meta: [
+        { name: 'viewport', content: 'width=device-width, initial-scale=1, maximum-scale=1' }
+      ],
+      bodyAttrs: {
+        class: 'bg-gray-200'
+      }
+});
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="p-5">
+    HOME
+  </div>
+</template>
+
+<script lang="ts" script>
+useMeta({
+  title: 'Home'
+});
+</script>
